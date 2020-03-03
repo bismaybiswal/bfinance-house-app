85f757814f9f86382a52ce1b4b115e121fa28495
Bismay Biswal <45811843+bismaybiswal@users.noreply.github.com>
2020-03-03 23:33:39 +0530

    Update expense-details.component.ts

--- src/app/pages/expense-details/expense-details.component.ts
+++ src/app/pages/expense-details/expense-details.component.ts
@@ -64,7 +64,7 @@ export class ExpenseDetailsComponent implements OnInit {
       amount: ['', Validators.compose([Validators.required])],
       desc: ['', Validators.compose([Validators.required])],
       transactionDate: ['', Validators.compose([Validators.required])],
-      category: ['', Validators.compose([Validators.required])]
+      category: ['OTHERS']
     });
   }
 
